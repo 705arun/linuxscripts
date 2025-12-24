471f8590d8e91f6b80c4bb09d3c6744ec83aa140
705arun <63182997+705arun@users.noreply.github.com>
2025-12-24 10:29:14 +0530

    Create echo.sh

---
+++ echo.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "hi I am in bangalore"
+echo "the weather is good"
+mkdir /home/osboxes/test/scripts/samsung
+echo "this is the new direcory" > /home/osboxes/test/scripts/samsung/testfile.txt
